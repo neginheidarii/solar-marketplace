16d4525d3f7f0908108dd9357276226fca8a82c5
neginheidarii <neginheidari@icloud.com>
2025-08-22 13:36:55 -0400

    replace SearchBar with ProductSearch component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
+import ProductSearch from "@/components/ProductSearch";
 import getProducts from "@/lib/api";
-import type { Product } from "@/types";
-import SearchBar from "@/components/SearchBar";
 
 export default async function Home() {
-  const products: Product[] = await getProducts();
+  const products = await getProducts();
+
   return (
     <div className="max-w-6xl mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">Product List</h1>
-      <SearchBar />
+      <ProductSearch products={products} />
     </div>
   );
 }
